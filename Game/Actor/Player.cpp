ac231bbf0e9e96dae08b8c6ee8d2c949d1d38bae
kmklko453-cmyk <kmklko453@gmail.com>
2026-02-04 12:11:27 +0900

    플레이어 탄약 발사 기본 로직 구현
    - PlayerBullet 클래스 추가
      - 이동 로직 처리 (float->int)
    - Actor에 헬퍼 함수 추가
      - Destroy / OnDestroy / QuitGame 추가

--- Game/Actor/Player.cpp
+++ Game/Actor/Player.cpp
@@ -1,6 +1,9 @@
 #include "Player.h"
+#include "Actor/PlayerBullet.h"
 #include "Core/Input.h"
 #include "Engine/Engine.h"
+#include "Level/Level.h"
+
 
 Player::Player()
 	: super ("<=A=>",Vector2::Zero, Color::Green)
@@ -19,6 +22,13 @@ void Player::Tick(float deltaTime)
 {	
 	super::Tick(deltaTime);
 
+	//종료 처리
+	if (Input::Get().GetKeyDown(VK_ESCAPE))
+	{
+		QuitGame();
+	}
+
+
 	//좌우 방향기 입력처리
 	if (Input::Get().GetKey(VK_LEFT))
 	{
@@ -29,7 +39,11 @@ void Player::Tick(float deltaTime)
 		MoveRight();
 	}
 
-
+	//스페이스 키를 활용해 탄약 발사
+	if (Input::Get().GetKey(VK_SPACE))
+	{
+		Fire();
+	}
 
 }
 
@@ -58,4 +72,13 @@ void Player::MoveLeft()
 		position.x = 0;
 	}
 
+}
+
+void Player::Fire()
+{
+	// 위치 설정
+	Vector2 bulletPosition(position.x + (width / 2), position.y);
+
+	// 액터 설정
+	GetOwner()->AddNewActor(new PlayerBullet(bulletPosition));
 }
